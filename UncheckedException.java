7c16faf88bec4d57afe4a4a73e0640e481ae44a2
riturai06 <59560369+riturai06@users.noreply.github.com>
2020-02-08 07:22:00 +0530

    Add files via upload

---
+++ UncheckedException.java
@@ -0,0 +1,22 @@
+/*Author: Ritu Bala Rai
+ *Description: Handling Unchecked Exception.
+ * */
+
+package ygg;
+
+public class UncheckedException {
+
+	public static void main(String[] args) {
+		
+		 
+		    
+		        int x = 10/0;
+		    // Arithmetic Exception
+		         System.out.println("Exception: " +x);// Unchecked Exception handle by jvm , we can also handle by usin try-catch..
+		   
+		   
+		
+
+	}
+
+}
