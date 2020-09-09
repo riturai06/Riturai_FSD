953fff84e8d4aad249f267bb08c691de1b91692c
riturai06 <59560369+riturai06@users.noreply.github.com>
2020-09-09 13:46:26 +0530

    Update home.component.ts

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserserviceService } from '../userservice.service';
-import { Usermodel } from '../user';
+
 
 @Component({
   selector: 'app-home',
@@ -9,23 +8,13 @@ import { Usermodel } from '../user';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  username: string;
-  user: Usermodel[];
-  isLoggedIn:boolean = false;
-  token:string;
 
 
-  constructor(private userService: UserserviceService,private route : Router) { }
+  constructor() { }
 
   ngOnInit(): void {
     
-    this.username= window.localStorage.getItem('username');
-    this.token = window.localStorage.getItem('token');
-    if(this.token != null && this.token.length > 10){
-      this.isLoggedIn = true;
-    }
-    let resp = this.userService.getUser();
-    resp.subscribe((data) =>{this.user= data;console.log('user',data)});
+    
   }
 
 }
